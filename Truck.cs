69a1bbc0b60218011066e9b69965169d067ee67c
BretShep03 <bretshep03@gmail.com>
2023-02-01 09:12:42 -0600

    Create Truck.cs

---
+++ Truck.cs
@@ -0,0 +1,8 @@
+public class Truck
+{
+	public string Name { get; set; }
+	public string Make { get; set; }
+	public int Year { get; set; }
+	public string Color { get; set; }
+	public string Horsepower {get; set; }
+}
