1836f4aee5ec2f6c9839e54821cc329afcadaef8
alang2002 <aaronslang@outlook.com>
2023-02-08 08:44:43 -0600

    Added singletons for services

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<AutoDatabaseSettings>(builder.Configuration.GetSection(nameof(AutoDatabaseSettings.DatabaseName)));
+builder.Services.AddSingleton<CarService>();
+builder.Services.AddSingleton<MotorcycleService>();
 builder.Services.AddSingleton<TruckService>();
 
 builder.Services.AddControllers()
@@ -13,6 +15,12 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/*builder.WebHost.UseKestrel(serverOptions =>
+{
+    //serverOptions.ListenAnyIP(7273);
+    serverOptions.ListenAnyIP(7272, listenOptions => listenOptions.UseHttps());
+});*/
 
 var app = builder.Build();
 
